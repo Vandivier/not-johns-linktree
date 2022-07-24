b9493df5c07dbeefdcff3c8672efde06e3a34f05
Vandivier <john@afterecon.com>
2022-07-24 19:47:04 -0400

    fix: asset out location

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  assetPrefix: !debug ? "/basic-nextjs-portfolio/" : "",
+  assetPrefix: !debug ? "/not-johns-linktree/" : "",
   // images: { loader: "imgix" },
   // reactStrictMode: true,
 };
