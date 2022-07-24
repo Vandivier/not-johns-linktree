44e23f93ce1dba93ab4722ce3aff39da45028c9b
Vandivier <john@afterecon.com>
2022-07-24 19:44:04 -0400

    Deploy Next.js to gh-pages

--- _next/static/Vkb6x_FsrlO1XgPZc7gn4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1522af19d675d48d5e10.css","static/chunks/pages/index-c5e65770bb35cfeb08b6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
