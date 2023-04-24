e9a8ce36ae2e431b6dcbef2c536fd2a9a58ab0b0
Vandivier <vandivi3r@gmail.com>
2023-04-23 22:20:52 -0600

    feat: CustomIconData

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,7 +1,18 @@
 import { FunctionComponent } from "react";
 import { SocialIcon, SocialIconProps } from "react-social-icons";
 
+import React from "react";
 import styles from "./Socials.module.css";
+import { Mastodon } from "./icons/mastadon";
+import { Substack } from "./icons/Substack";
+
+type CustomIconData = {
+  backgroundColor: string;
+  height: string;
+  url: string;
+  svg: React.FC<React.SVGProps<SVGSVGElement>>;
+  width: string;
+};
 
 const StyledSocialIcon: FunctionComponent<SocialIconProps> = (props) => (
   <SocialIcon
@@ -10,12 +21,24 @@ const StyledSocialIcon: FunctionComponent<SocialIconProps> = (props) => (
   />
 );
 
-const orderedSocials = [
+const orderedSocials: (string | CustomIconData)[] = [
   "https://www.youtube.com/c/JohnVandivier",
   "https://www.tiktok.com/@johnvandivier",
-  "https://optimality.substack.com/",
+  {
+    backgroundColor: "#FF681A",
+    height: "16px",
+    svg: Substack,
+    url: "https://optimality.substack.com/",
+    width: "14px",
+  },
   "https://twitter.com/johnvandivier",
-  "https://mastodon.online/@johnvandivier",
+  {
+    backgroundColor: "#8c8dfe",
+    height: "20px",
+    svg: Mastodon,
+    url: "https://mastodon.online/@johnvandivier",
+    width: "18px",
+  },
   "https://twitch.tv/johnvandivier",
   "https://discord.gg/fAg6Xa4uxc",
   "https://www.instagram.com/vandivi3r/",
@@ -24,12 +47,48 @@ const orderedSocials = [
   "mailto:john@ladderly.io",
 ];
 
+const SocialItemWithCustomIcon: FunctionComponent<{ data: CustomIconData }> = ({
+  data,
+}) => (
+  <li
+    key={data.url}
+    style={{
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+      width: "40px",
+    }}
+  >
+    <div
+      className={styles["social-circle"]}
+      style={{ backgroundColor: data.backgroundColor }}
+    >
+      <a
+        href={data.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ height: data.height, width: data.width }}
+      >
+        <data.svg />
+      </a>
+    </div>
+  </li>
+);
+
+const SocialItemFromString: FunctionComponent<{ url: string }> = ({ url }) => (
+  <li key={url}>
+    <StyledSocialIcon url={url} target="_blank" />
+  </li>
+);
+
 export const Socials = () => (
   <ul className={styles["social-list-wrapper"]}>
-    {orderedSocials.map((url) => (
-      <li key={url}>
-        <StyledSocialIcon url={url} target="_blank" />
-      </li>
-    ))}
+    {orderedSocials.map((data) =>
+      typeof data === "string" ? (
+        <SocialItemFromString url={data} />
+      ) : (
+        <SocialItemWithCustomIcon data={data} />
+      )
+    )}
   </ul>
 );
---
+++ components/icons/Substack.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// credit: https://uxwing.com/substack-icon/
+export const Substack: React.FC<React.SVGProps<SVGSVGElement>> = () => (
+  <svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    <path
+      fill="#FF681A"
+      d="M0 0h448v62.804H0V0zm0 229.083h448v282.388L223.954 385.808 0 511.471V229.083zm0-114.542h448v62.804H0v-62.804z"
+    ></path>
+  </svg>
+);
