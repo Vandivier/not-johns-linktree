a25494e374c343948190bfbd3bfaba94948a2a41
Vandivier <vandivi3r@gmail.com>
2023-08-19 14:26:52 -0400

    feat: intuitive ternary

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  assetPrefix: !debug ? "/not-johns-linktree/" : "",
+  assetPrefix: debug ? "": "/not-johns-linktree/",
   // images: { loader: "imgix" },
   // reactStrictMode: true,
 };
