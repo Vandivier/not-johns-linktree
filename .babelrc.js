3fe9f143c8e52ba2c6be12a40bc1b06a5581c4d7
Vandivier <vandivi3r@gmail.com>
2023-08-19 14:36:33 -0400

    feat: rm env-config file

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,10 @@
-const env = require('./env-config')
+const prod = process.env.NODE_ENV === "production";
+
+const env = {
+  "process.env.BACKEND_URL": prod ? "/basic-nextjs-portfolio" : "",
+};
 
 module.exports = {
-  presets: ['next/babel'],
-  plugins: [['transform-define', env]],
-}
+  presets: ["next/babel"],
+  plugins: [["transform-define", env]],
+};
