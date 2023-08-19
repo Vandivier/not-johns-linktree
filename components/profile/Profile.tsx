0fb7c5095a2ee16659e880c907ea2e0d6ccbc1df
Vandivier <vandivi3r@gmail.com>
2023-08-19 14:27:15 -0400

    feat: use next image

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./Profile.module.css";
 
 export const Profile = ({ path }: { path: string }) => {
   return (
     <div style={{ display: "flex" }}>
-      <img className={styles["profile-image"]} src={path} alt="Profile" />
+      <Image className={styles["profile-image"]} src={path} alt="Profile" />
     </div>
   );
 };
